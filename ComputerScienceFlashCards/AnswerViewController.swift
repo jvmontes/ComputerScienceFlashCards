446ce0dbe039a7077b6581db748a0b8245551f52
Jorge Viramontes <jviramontes@solstice-mobile.com>
2017-03-08 21:43:35 -0600

    WIP: Added answer copy and about to create mapping from question to answer. Can either 1) Store a local string to save selected response or 2) Send QuestionSet information in QuestionCell, the segue\'s sender. QuestionSet could be a protocol that implements an array of dictionaries with questions and answers. Can add additional information such as image, web address or video.

---
+++ ComputerScienceFlashCards/AnswerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AnswerViewController.swift
+//  ComputerScienceFlashCards
+//
+//  Created by Jorge Viramontes on 3/8/17.
+//  Copyright © 2017 Solstice. All rights reserved.
+//
+
+import UIKit
+
+class AnswerViewController: UIViewController {
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+}
+
